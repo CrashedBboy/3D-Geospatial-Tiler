96bf4b28c311c262ff6c97d494c614cfcf0366c0
CrashedBboy <crashedbboy@gmail.com>
2019-03-28 11:29:40 +0800

    quad-tile: fix bugs of not fully separating faces

--- scripts/quad-tile.py
+++ scripts/quad-tile.py
@@ -55,64 +55,66 @@ visulization:
 
 # select polygons and separate them from original object
 if target:
-    tile_0_0_vertices = [] # tile 2
-    tile_0_1_vertices = [] # tile 0
-    tile_1_0_vertices = [] # tile 3
-    tile_1_1_vertices = [] # tile 1
-    # catergorize vertices
-    for v in target.data.vertices:
-        if v.co.x >= center[0] and v.co.y >= center[1]:
-            tile_0_1_vertices.append(v.index)
-        if v.co.x < center[0] and v.co.y >= center[1]:
-            tile_0_0_vertices.append(v.index)
-        if v.co.x < center[0] and v.co.y < center[1]:
-            tile_1_0_vertices.append(v.index)
-        if v.co.x >= center[0] and v.co.y < center[1]:
-            tile_1_1_vertices.append(v.index)
-    tile_0_0_polygons = [] # tile 2
-    tile_0_1_polygons = [] # tile 0
-    tile_1_0_polygons = [] # tile 3
-    tile_1_1_polygons = [] # tile 1
-    # catergorize polygons
-    for p in target.data.polygons:
-        tile = 3 # which tile this polygon belongs to
-        for pv in p.vertices:
-            if pv in tile_0_0_vertices:
-                tile = min(tile, 2)
-                continue
-            elif pv in tile_0_1_vertices:
-                tile = min(tile, 0)
-                continue
-            elif pv in tile_1_0_vertices:
-                tile = min(tile, 3)
-                continue
-            elif pv in tile_1_1_vertices:
-                tile = min(tile, 1)
-        if tile == 0:
-            tile_0_1_polygons.append(p.index)
-            continue
-        elif tile == 1:
-            tile_1_1_polygons.append(p.index)
-            continue
-        elif tile == 2:
-            tile_0_0_polygons.append(p.index)
-            continue
-        elif tile == 3:
-            tile_1_0_polygons.append(p.index)
-    tiles_polygons = [tile_0_0_polygons, tile_0_1_polygons, tile_1_0_polygons, tile_1_1_polygons]
-    tiles_names = ['tile_0_0', 'tile_0_1', 'tile_1_0', 'tile_1_1']
+
+    # enter EDIT mode so that bmesh can work
     bpy.ops.object.mode_set(mode='EDIT')
     bm = bmesh.from_edit_mesh(target.data)
-    for name, polygons in zip(tiles_names, tiles_polygons):
-        print(len(polygons))
-        bpy.ops.mesh.select_all(action='DESELECT')
-        selected = False
-        for face in bm.faces:
-            if face.index in polygons:
-                if not selected:
-                    selected = True
-                face.select_set(True)
-        bmesh.update_edit_mesh(target.data, True)
-        if (selected):
-            bpy.ops.mesh.separate(type='SELECTED')
+
+    # select polygons for tile_0_1
+    selected = 0
+    bpy.ops.mesh.select_all(action='DESELECT')
+    for face in bm.faces:
+        should_select = False
+        for v in face.verts:
+            if v.co.x >= center[0] and v.co.y >= center[1]:
+                should_select = True
+                break
+        if (should_select):
+            face.select_set(True)
+            selected += 1
+    bmesh.update_edit_mesh(target.data, True)
+    if (selected > 0):
+        bpy.ops.mesh.separate(type='SELECTED')
+
+    # select polygons for tile_1_1
+    selected = 0
+    bpy.ops.mesh.select_all(action='DESELECT')
+    for face in bm.faces:
+        should_select = False
+        for v in face.verts:
+            if v.co.x >= center[0]:
+                should_select = True
+                break
+        if (should_select):
+            face.select_set(True)
+            selected += 1
+    bmesh.update_edit_mesh(target.data, True)
+    if (selected > 0):
+        bpy.ops.mesh.separate(type='SELECTED')
+    
+    # select polygons for tile_0_0
+    selected = 0
+    bpy.ops.mesh.select_all(action='DESELECT')
+    for face in bm.faces:
+        should_select = False
+        for v in face.verts:
+            if v.co.y >= center[1]:
+                should_select = True
+                break
+        if (should_select):
+            face.select_set(True)
+            selected += 1
+    bmesh.update_edit_mesh(target.data, True)
+    if (selected > 0):
+        bpy.ops.mesh.separate(type='SELECTED')
+    
+    # select polygons for tile_1_0
+    selected = 0
+    bpy.ops.mesh.select_all(action='DESELECT')
+    for face in bm.faces:
+        face.select_set(True)
+        selected += 1
+    bmesh.update_edit_mesh(target.data, True)
+    if (selected > 0):
+        bpy.ops.mesh.separate(type='SELECTED')
     bpy.ops.object.mode_set(mode='OBJECT')
