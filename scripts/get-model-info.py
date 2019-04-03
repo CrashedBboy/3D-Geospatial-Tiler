8e1ed2c1618cf7e15cf71bb016d0045ee4be0210
CrashedBboy <crashedbboy@gmail.com>
2019-04-03 09:59:11 +0800

    fix error in getting texture image file size

--- scripts/get-model-info.py
+++ scripts/get-model-info.py
@@ -24,20 +24,23 @@ textures_size = 0 # in bytes
 geometry_size = None # in bytes
 
 for obj in bpy.data.objects:
-    vertices_count += len(obj.data.vertices)
-    polygons_count += len(obj.data.polygons)
-    for uv_map in obj.data.uv_layers:
-        uv_count += len(uv_map.data)
+    if obj.type == "MESH":
+        vertices_count += len(obj.data.vertices)
+        polygons_count += len(obj.data.polygons)
+
+for uv_map in obj.data.uv_layers:
+    uv_count += len(uv_map.data)
 
 print("vertices_count: ", vertices_count)
 print("polygons_count: ", polygons_count)
 print("uv_count: ", uv_count)
 
 for image in bpy.data.images:
-    if image.filepath != '':
-        file_size = os.path.getsize(image.filepath) # in bytes
-        textures_size += file_size
-        textures.append({'path': image.filepath, 'file_size': file_size, 'width': image.size[0], 'height': image.size[1]})
+    if (image.filepath != '') and (image.type == 'IMAGE'):
+        if os.path.exists(image.filepath):
+            file_size = os.path.getsize(image.filepath) # in bytes
+            textures_size += file_size
+            textures.append({'path': image.filepath, 'file_size': file_size, 'width': image.size[0], 'height': image.size[1]})
 
 print("all textures: \n", str(textures))
 print("total texture size(bytes): \n", str(textures_size))
